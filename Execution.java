4191f8d0e0dc97fe71f349590d259b85f6e9992f
dxr5716 <dxr5716@rit.edu>
2014-11-08 16:38:58 -0500

    Execution class

---
+++ Execution.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Execution {
+   
+   public static void main(String[] args) {
+      
+      ArrayList<Resources> resourcesList = new ArrayList<Resources>(); ; 
+      ArrayList<Survivor> survivorsList = new ArrayList<Survivor>();
+   }
+
+}
