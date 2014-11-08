8e88ec65bb5f1baa6db2e5e79540d047b405a442
Philip Bedward Jr. <philipbed813@gmail.com>
2014-11-08 17:53:06 -0500

    Donation class

---
+++ Donation.java
@@ -0,0 +1,23 @@
+
+public class Donation {
+	private String name;
+	private String type;
+	private String amount;
+	
+	public Donation(String name, String type, String amount) {
+		this.name = name;
+		this.type = type;
+		this.amount = amount;
+		
+	}
+	public getName() {
+		return name;
+		
+	}
+	public getType() {
+		return type
+	}
+	public getAmount() {
+		return amount
+	}
+}
